ba70056ee0304de4b353b415c9f41dd1d568a47b
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-11 16:10:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,15 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @students = Student.all 
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def delete
+  end
+
+  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'students/index'
+
+  get 'students/show'
+
+  get 'students/delete'
+
+  get 'students/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+ resources :students, only: [:index, :show] 
 end
